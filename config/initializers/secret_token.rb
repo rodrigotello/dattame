05391cee10bdc350d1d3fd722ad49a8543cf7448
rodrigotello <rodrigotello@outlook.com>
2014-04-30 02:58:51 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dattame::Application.config.secret_token = 'f1cd7fadbaf2099c8ebf4f9382dc757552acd46647b65645cd9ea4fba754f5fc85b37f8a56c9b9248dbf4f4efbc6bdf437151c48765b88ce344a4bcc0173b395'
